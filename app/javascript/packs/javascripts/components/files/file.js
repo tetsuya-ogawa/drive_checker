4232969dada3e822e91100c9e79d159718282b1a
PT1972 <tetsuya.ogawa87@gmail.com>
2018-07-11 18:43:59 +0900

    パーミッション情報取り出し

--- app/javascript/packs/javascripts/components/files/file.js
+++ app/javascript/packs/javascripts/components/files/file.js
@@ -3,10 +3,32 @@ import React from 'react'
 export default function File({file, hoge}) {
     const ownerName = file.owners[0].display_name
     const ownerEmail = file.owners[0].email_address
+    let permissionsWithType = (file.permissions || []).reduce((result, current) => {
+        if(!result[current.type]) result[current.type] = []
+        result[current.type].push(current)
+        return result
+    }, {})
+
+    const jampFileLink = link => {
+        window.open(link);
+    }
+
+    const permissionItems = permissions => {
+        if(!permissions) return 'なし'
+        return permissions.map( (permission, idx) => <div key={file.id + permission.id}><strong>{permission.role}</strong> - {permission.display_name} ({permission.email_address})</div> )
+    }
+
     return (
-        <div onClick={() => hoge(file)}>
-            <h3>{file.name}</h3>
-            <div>{ownerName} ({ownerEmail})</div>
+        <div className="c-file">
+            <div className="c-file__fileName" onClick={() => jampFileLink(file.web_view_link)}>
+                <h3>{file.name}</h3>
+            </div>
+            <div>
+                <h4>共有ユーザー(ドメイン)</h4>
+                {permissionItems(permissionsWithType['domain'])}
+                <h4>共有ユーザー(ユーザー)</h4>
+                {permissionItems(permissionsWithType['user'])}
+            </div>
         </div>
     )
 }
