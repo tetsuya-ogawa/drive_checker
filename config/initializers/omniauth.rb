b9ae7837b017593547a1bb6e3ed600c3c09341d9
PT1972 <tetsuya.ogawa87@gmail.com>
2018-07-11 17:04:03 +0900

    本番環境で認可できるように

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     scope: 'userinfo.email, userinfo.profile, drive.metadata',
-    prompt: 'select_account',
+    prompt: 'consent',
   }
 end
